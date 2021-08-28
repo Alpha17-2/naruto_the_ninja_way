7f5dd2eca3a878dd57b5a5188acc39ddec716f59
Alpha17-2 <alphaisgod1@gmail.com>
2021-08-28 16:14:13 +0530

    clan model implemented

---
+++ lib/Models/clan.dart
@@ -0,0 +1,8 @@
+class Clan {
+  final String clanName;
+  final String description;
+  final Map<String, String> members;
+  final String village;
+  final String signatureAbility;
+  Clan({this.clanName, this.village,this.signatureAbility, this.members, this.description});
+}
